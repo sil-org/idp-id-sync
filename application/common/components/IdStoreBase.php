95b0852e095a67173f6c0ea0b9af8e9c2e012b4b
Matt Henderson <matt_henderson@sil.org>
2017-03-31 11:57:58 -0400

    Clean up process for translating field names.

--- application/common/components/IdStoreBase.php
+++ application/common/components/IdStoreBase.php
@@ -6,32 +6,42 @@ use Sil\Idp\IdSync\common\interfaces\IdStoreInterface;
 
 abstract class IdStoreBase extends Component implements IdStoreInterface
 {
+    const ID_BROKER_DISPLAY_NAME = 'display_name';
+    const ID_BROKER_EMAIL = 'email';
+    const ID_BROKER_EMPLOYEE_ID = 'employee_id';
+    const ID_BROKER_FIRST_NAME = 'first_name';
+    const ID_BROKER_LAST_NAME = 'last_name';
+    const ID_BROKER_LOCKED = 'locked';
+    const ID_BROKER_USERNAME = 'username';
+    
     /**
-     * ID Broker field names, indexed by equivalent ID Store field names.
+     * Get the list of ID Broker field names, indexed by the equivalent ID Store
+     * field names.
      *
      * @var array<string,string>
      */
-    protected $idBrokerFieldNames = [
-        'employeeNumber' => 'employee_id',
-        'firstName' => 'first_name',
-        'lastName' => 'last_name',
-        'displayName' => 'display_name',
-        'email' => 'email',
-        'username' => 'username',
-        'locked' => 'locked',
-    ];
+    abstract public static function getIdBrokerFieldNames();
     
-    protected function getIdBrokerFieldName(string $idStoreFieldName)
+    protected function getIdBrokerFieldNameFor(string $idStoreFieldName)
     {
-        return $this->idBrokerFieldNames[$idStoreFieldName];
+        $idBrokerFieldNames = static::getIdBrokerFieldNames();
+        return $idBrokerFieldNames[$idStoreFieldName];
     }
     
+    /**
+     * Take the given array of user info and translate the keys from the field
+     * names used by the ID Store to those used by the ID Broker.
+     *
+     * @param array $userFromIdStore
+     * @return array The array of user information, keyed on the ID Broker
+     *     version of the field names.
+     */
     protected function translateToIdBrokerFieldNames(array $userFromIdStore)
     {
         $userForIdBroker = [];
         
         foreach ($userFromIdStore as $idStoreFieldName => $value) {
-            $idBrokerFieldName = $this->getIdBrokerFieldName($idStoreFieldName);
+            $idBrokerFieldName = $this->getIdBrokerFieldNameFor($idStoreFieldName);
             $userForIdBroker[$idBrokerFieldName] = $value;
         }
         
