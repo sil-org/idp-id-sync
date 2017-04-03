ce5fca0e1e06ae324e3471e182a02382622991aa
Matt Henderson <matt_henderson@sil.org>
2017-04-03 11:03:12 -0400

    Add doc. comment to `syncUser(...)`.

--- application/common/sync/Synchronizer.php
+++ application/common/sync/Synchronizer.php
@@ -105,6 +105,12 @@ class Synchronizer
         }
     }
     
+    /**
+     * Synchronize a specific user, requesting their information from the
+     * ID Store and updating it accordingly in the ID Broker.
+     *
+     * @param string $employeeId The EmployeeID of the user to sync.
+     */
     public function syncUser($employeeId)
     {
         $idStoreUser = $this->idStore->getActiveUser($employeeId);
