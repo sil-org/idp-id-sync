13b3a779d80e97cff8fc12b6ea0ef4b953cf32f3
Matt Henderson <matt_henderson@sil.org>
2017-04-13 17:11:24 -0400

    Update ID Store/Broker config to handle optional entries.

--- application/common/config/main.php
+++ application/common/config/main.php
@@ -3,25 +3,43 @@
 use Sil\Idp\IdSync\common\components\IdBrokerBase;
 use Sil\Idp\IdSync\common\components\IdStoreBase;
 use Sil\PhpEnv\Env;
+use yii\helpers\ArrayHelper;
+
+$idBrokerOptionalConfig = [];
+if (Env::get('ID_BROKER_ACCESS_TOKEN') !== null) {
+    $idBrokerOptionalConfig['accessToken'] = Env::get('ID_BROKER_ACCESS_TOKEN');
+}
+if (Env::get('ID_BROKER_BASE_URL') !== null) {
+    $idBrokerOptionalConfig['baseUrl'] = Env::get('ID_BROKER_BASE_URL');
+}
+
+$idStoreOptionalConfig = [];
+if (Env::get('ID_STORE_API_KEY') !== null) {
+    $idStoreOptionalConfig['apiKey'] = Env::get('ID_STORE_API_KEY');
+}
+if (Env::get('ID_STORE_API_SECRET') !== null) {
+    $idStoreOptionalConfig['apiSecret'] = Env::get('ID_STORE_API_SECRET');
+}
+if (Env::get('ID_STORE_BASE_URL') !== null) {
+    $idStoreOptionalConfig['baseUrl'] = Env::get('ID_STORE_BASE_URL');
+}
 
 return [
     'id' => 'id-sync',
     'components' => [
-        'idBroker' => [
+        
+        'idBroker' => ArrayHelper::merge([
             'class' => IdBrokerBase::getAdapterClassFor(
                 Env::get('ID_BROKER_ADAPTER')
             ),
-            'accessToken' => Env::get('ID_BROKER_ACCESS_TOKEN'),
-            'baseUrl' => Env::get('ID_BROKER_BASE_URL'),
-        ],
-        'idStore' => [
+        ], $idBrokerOptionalConfig),
+        
+        'idStore' => ArrayHelper::merge([
             'class' => IdStoreBase::getAdapterClassFor(
                 Env::get('ID_STORE_ADAPTER')
             ),
-            'apiKey' => Env::get('ID_STORE_API_KEY'),
-            'apiSecret' => Env::get('ID_STORE_API_SECRET'),
-            'baseUrl' => Env::get('ID_STORE_BASE_URL'),
-        ],
+        ], $idStoreOptionalConfig),
+        
         'log' => [
         ],
     ],
