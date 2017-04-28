67e872896c66cfe317853d42580f41f4f75c045f
Matt Henderson <matt_henderson@sil.org>
2017-04-28 11:05:53 -0400

    Refactor internal classes for updated php client interface.

--- application/common/interfaces/IdBrokerInterface.php
+++ application/common/interfaces/IdBrokerInterface.php
@@ -3,11 +3,12 @@ namespace Sil\Idp\IdSync\common\interfaces;
 
 interface IdBrokerInterface
 {
-    public function authenticate(array $config = []);
+    public function activateUser(string $employeeId);
+    public function authenticate(string $username, string $password);
     public function createUser(array $config = []);
-    public function deactivateUser(array $config = []);
-    public function getUser(array $config = []);
-    public function listUsers(array $config = []);
-    public function setPassword(array $config = []);
+    public function deactivateUser(string $employeeId);
+    public function getUser(string $employeeId);
+    public function listUsers($fields = null);
+    public function setPassword(string $employeeId, string $password);
     public function updateUser(array $config = []);
 }
