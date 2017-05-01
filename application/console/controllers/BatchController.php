1f8da4c7781bc8c677f546a5bb1ecc07f727ba7b
Matt Henderson <matt_henderson@sil.org>
2017-05-01 14:05:22 -0400

    Update BatchController actions to actually run syncs.

--- application/console/controllers/BatchController.php
+++ application/console/controllers/BatchController.php
@@ -1,20 +1,25 @@
 <?php
 namespace Sil\Idp\IdSync\console\controllers;
 
+use Sil\Idp\IdSync\common\sync\Synchronizer;
+use Yii;
 use yii\console\Controller;
 
 class BatchController extends Controller
 {
     public function actionFull()
     {
-        // TEMP
-        echo "Full\n";
+        $synchronizer = new Synchronizer(Yii::$app->idStore, Yii::$app->idBroker);
+        $synchronizer->syncAll();
     }
     
+    /**
+     * Run an incremental sync, slightly overlapping with the time frame of the
+     * previous sync.
+     */
     public function actionIncremental()
     {
-        // TEMP
-        echo "Incremental\n";
-        throw new \Exception('Testing error output.');
+        $synchronizer = new Synchronizer(Yii::$app->idStore, Yii::$app->idBroker);
+        $synchronizer->syncUsersChangedSince(strtotime('-11 minutes'));
     }
 }
