dd0e9fd49c5ce6110738f8045bb939b234ec30a4
Matt Henderson <matt_henderson@sil.org>
2017-05-01 14:17:11 -0400

    Switch APP_ID to IDP_NAME in full batch cron log content.

--- application/run-full-batch.sh
+++ application/run-full-batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-logger -p 1 -t batch.warning "{\"message\": \"Preparing to run ID Sync batch\", \"batch\": \"full\", \"app_id\": \"${APP_ID}\", \"app_env\": \"${APP_ENV}\"}"
+logger -p 1 -t batch.warning "{\"message\": \"Preparing to run ID Sync batch\", \"batch\": \"full\", \"idp_name\": \"${IDP_NAME}\", \"app_env\": \"${APP_ENV}\"}"
 
 /data/yii batch/full
 
-logger -p 1 -t batch.warning "{\"message\": \"Ran ID Sync batch\", \"batch\": \"full\", \"app_id\": \"${APP_ID}\", \"app_env\": \"${APP_ENV}\"}"
+logger -p 1 -t batch.warning "{\"message\": \"Ran ID Sync batch\", \"batch\": \"full\", \"idp_name\": \"${IDP_NAME}\", \"app_env\": \"${APP_ENV}\"}"
