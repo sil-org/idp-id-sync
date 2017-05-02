eb84fa872aa46ed66486c1c6c8e13f6f2a7277be
Matt Henderson <matt_henderson@sil.org>
2017-05-02 13:45:45 -0400

    Refactor interfaces to use new User value object.

--- application/common/interfaces/IdBrokerInterface.php
+++ application/common/interfaces/IdBrokerInterface.php
@@ -1,14 +1,84 @@
 <?php
 namespace Sil\Idp\IdSync\common\interfaces;
 
+use Sil\Idp\IdSync\common\models\User;
+
 interface IdBrokerInterface
 {
+    /**
+     * Activate a user.
+     *
+     * @param string $employeeId The Employee ID of the user to activate.
+     * @throws Exception
+     */
     public function activateUser(string $employeeId);
+    
+    /**
+     * Attempt to authenticate using the given credentials, getting back
+     * information about the authenticated user (if the credentials were
+     * acceptable) or null (if unacceptable).
+     *
+     * @param string $username The username.
+     * @param string $password The password (in plaintext).
+     * @return User|null User information (if valid), or null.
+     * @throws Exception
+     */
     public function authenticate(string $username, string $password);
+    
+    /**
+     * Create a user with the given information.
+     *
+     * @param array $config An array key/value pairs of attributes for the new
+     *     user.
+     * @return User User information.
+     * @throws Exception
+     */
     public function createUser(array $config = []);
+    
+    /**
+     * Deactivate a user.
+     *
+     * @param string $employeeId The Employee ID of the user to deactivate.
+     * @throws Exception
+     */
     public function deactivateUser(string $employeeId);
+    
+    /**
+     * Get information about the specified user.
+     *
+     * @param string $employeeId The Employee ID of the desired user.
+     * @return User|null An array of information about the specified user, or
+     *     null if no such user was found.
+     * @throws Exception
+     */
     public function getUser(string $employeeId);
+    
+    /**
+     * Get a list of all users.
+     *
+     * @param array|null $fields (Optional:) The list of fields desired about
+     *     each user in the result.
+     * @return User[] A list of Users.
+     */
     public function listUsers($fields = null);
+    
+    /**
+     * Set the password for the specified user.
+     *
+     * @param string $employeeId The Employee ID of the user whose password we
+     *     are trying to set.
+     * @param string $password The desired (new) password, in plaintext.
+     * @throws Exception
+     */
     public function setPassword(string $employeeId, string $password);
+    
+    /**
+     * Update the specified user with the given information.
+     *
+     * @param array $config An array key/value pairs of attributes for the user.
+     *     Must include at least an 'employee_id' entry.
+     * @return User Information about the updated user.
+     * @throws Exception
+     */
     public function updateUser(array $config = []);
 }
--- application/common/interfaces/IdStoreInterface.php
+++ application/common/interfaces/IdStoreInterface.php
@@ -1,34 +1,33 @@
 <?php
 namespace Sil\Idp\IdSync\common\interfaces;
 
+use Sil\Idp\IdSync\common\models\User;
+
 interface IdStoreInterface
 {
     /**
-     * Get the specified user's information (using ID Broker field names). Note
-     * that inactive users will be treated as non-existent users.
+     * Get the specified user's information. Note that inactive users will be
+     * treated as non-existent users.
      *
      * @param string $employeeId The Employee ID.
-     * @return array|null Information about the specified user, or null if no
+     * @return User|null Information about the specified user, or null if no
      *     such active user was found.
      */
     public function getActiveUser(string $employeeId);
     
     /**
-     * Get information about each of the (active) users (using ID Broker field
-     * names).
+     * Get information about each of the (active) users.
      *
-     * @return array A list of user-information arrays.
+     * @return User[] A list of Users.
      */
     public function getAllActiveUsers();
     
     /**
      * Get a list of users who have had qualifying changes (name, email, locked,
-     * activated, added) since the given Unix timestamp. As with all the other
-     * functions, the results need to provide user info using ID Broker field
-     * names (not ID Store field names).
+     * activated, added) since the given Unix timestamp.
      * 
      * @param int $unixTimestamp The date/time, as a Unix timestamp.
-     * @return array A list of user-information arrays.
+     * @return User[] A list of Users.
      */
     public function getUsersChangedSince(int $unixTimestamp);
 }
