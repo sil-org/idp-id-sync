2cd4b21a8a1e06b869b4d7124c0c22e0ca25ac12
Matt Henderson <matt_henderson@sil.org>
2017-05-15 16:50:43 -0400

    Add template for missing-email notification.

---
+++ application/common/mail/missing-email.php
@@ -0,0 +1,30 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this \yii\web\View The view component instance. */
+/* @var $message \yii\mail\BaseMessage Newly created mail message. */
+/* @var $idStoreName string */
+/* @var $organizationName string */
+/* @var $employeeId string */
+/* @var $username string */
+/* @var $firstName string */
+/* @var $lastName string */
+/* @var $ourEmailAddress string */
+
+?>
+<h2>Missing Email</h2>
+<p>
+  The following <?= Html::encode($idStoreName) ?> user is missing an email 
+  address. Without this, they will be unable to log in to certain 
+  <?= Html::encode($organizationName) ?> websites.
+</p>
+<ul>
+  <li><b>Employee ID:</b> <?= Html::encode($employeeId) ?></li>
+  <li><b>Username:</b> <?= Html::encode($username) ?></li>
+  <li><b>First Name:</b> <?= Html::encode($firstName) ?></li>
+  <li><b>Last Name:</b> <?= Html::encode($lastName) ?></li>
+</ul>
+<p>
+  If you have any questions about this, you can contact us here: <br />
+  <?= Html::mailto(Html::encode($ourEmailAddress)) ?>
+</p>
