7d0abe3ec0cf5842fd370b86e5e6a7121999c99a
Matt Henderson <matt_henderson@sil.org>
2017-05-31 10:00:50 -0400

    Properly initialize GoogleSheetsIdStore (via constructor).

--- application/sheets-test.php
+++ application/sheets-test.php
@@ -1,12 +1,14 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/yiisoft/yii2/Yii.php';
 
 use Sil\Idp\IdSync\common\components\adapters\GoogleSheetsIdStore;
 use Sil\PhpEnv\Env;
 
-$store = new GoogleSheetsIdStore();
-$store->applicationName = Env::requireEnv('ID_STORE_applicationName');
-$store->jsonAuthString = Env::requireEnv('ID_STORE_jsonAuthString');
-$store->spreadsheetId = Env::requireEnv('ID_STORE_spreadsheetId');
+$store = new GoogleSheetsIdStore([
+    'applicationName' => Env::requireEnv('ID_STORE_applicationName'),
+    'jsonAuthString' => Env::requireEnv('ID_STORE_jsonAuthString'),
+    'spreadsheetId' => Env::requireEnv('ID_STORE_spreadsheetId'),
+]);
 
 var_dump($store->getAllActiveUsers());
