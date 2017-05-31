c8f8d55e1aabc29a8b493a02df96d82950ae49d3
Matt Henderson <matt_henderson@sil.org>
2017-05-31 13:40:24 -0400

    Update ID Store integration tests (Google Sheets, Insite).

--- application/features/bootstrap/InsiteIntegrationContext.php
+++ application/features/bootstrap/InsiteIntegrationContext.php
@@ -1,30 +1,19 @@
 <?php
 namespace Sil\Idp\IdSync\Behat\Context;
 
-use Behat\Behat\Tester\Exception\PendingException;
-use Behat\Gherkin\Node\TableNode;
-use Behat\Behat\Context\Context;
-use PHPUnit\Framework\Assert;
-use Sil\Idp\IdSync\common\sync\Synchronizer;
-use Sil\Idp\IdSync\common\components\adapters\fakes\FakeIdBroker;
+use Sil\Idp\IdSync\Behat\Context\IdStoreIntegrationContextBase;
 use Sil\Idp\IdSync\common\components\adapters\InsiteIdStore;
-use Sil\Idp\IdSync\common\models\User;
 use Sil\PhpEnv\Env;
-use yii\helpers\Json;
 
 /**
  * Defines application features from the specific context.
  */
-class InsiteIntegrationContext implements Context
+class InsiteIntegrationContext extends IdStoreIntegrationContextBase
 {
-    /** @var InsiteIdStore */
-    private $idStore;
-    
-    private $result;
-    
     public function __construct()
     {
-        require_once __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
+        echo 'Testing integration with Insite.' . PHP_EOL;
+        parent::__construct();
     }
     
     /**
@@ -50,54 +39,5 @@ class InsiteIntegrationContext implements Context
     {
         $employeeId = Env::requireEnv('TEST_INSITE_EMPLOYEE_ID');
         $this->result = $this->idStore->getActiveUser($employeeId);
-    }
-    
-    /**
-     * @Then I should get back information about that user
-     */
-    public function iShouldGetBackInformationAboutThatUser()
-    {
-        Assert::assertNotNull($this->result);
-        Assert::assertInstanceOf(User::class, $this->result);
-    }
-    
-    /**
-     * @When I ask the ID Store for all active users
-     */
-    public function iAskTheIdStoreForAllActiveUsers()
-    {
-        $this->result = $this->idStore->getAllActiveUsers();
-    }
-    
-    /**
-     * @Then I should get back a list of information about active users
-     */
-    public function iShouldGetBackAListOfInformationAboutActiveUsers()
-    {
-        Assert::assertNotNull($this->result);
-        Assert::assertNotEmpty($this->result);
-        foreach ($this->result as $user) {
-            Assert::assertInstanceOf(User::class, $user);
-        }
-    }
-    
-    /**
-     * @When I ask the ID Store for all users changed since a specific point in time
-     */
-    public function iAskTheIdStoreForAllUsersChangedSinceASpecificPointInTime()
-    {
-        $this->result = $this->idStore->getUsersChangedSince(1489764017);
-    }
-    
-    /**
-     * @Then I should get back a list of information about changed users
-     */
-    public function iShouldGetBackAListOfInformationAboutChangedUsers()
-    {
-        Assert::assertNotNull($this->result);
-        Assert::assertNotEmpty($this->result);
-        foreach ($this->result as $user) {
-            Assert::assertInstanceOf(User::class, $user);
-        }
     }
 }
