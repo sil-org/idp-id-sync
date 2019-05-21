91b381bfc97e26b7d4c29b449fc9e56000a202fe
Schparky <3172830+Schparky@users.noreply.github.com>
2019-05-21 14:17:11 -0600

    pull data from Sage query - getAllActiveUsers

--- application/common/components/adapters/SagePeopleIdStore.php
+++ application/common/components/adapters/SagePeopleIdStore.php
@@ -4,6 +4,7 @@ namespace Sil\Idp\IdSync\common\components\adapters;
 use Exception;
 use GuzzleHttp\Client;
 use InvalidArgumentException;
+use Sil\Idp\IdSync\common\components\helpers\Utils;
 use Sil\Idp\IdSync\common\components\IdStoreBase;
 use Sil\Idp\IdSync\common\models\User;
 use yii\helpers\Json;
@@ -51,18 +52,17 @@ class SagePeopleIdStore extends IdStoreBase
     public static function getIdBrokerFieldNames()
     {
         return [
-            'fHCM2__Team_Member__c.fHCM2__Unique_Id__c' => User::EMPLOYEE_ID,
-            'fHCM2__Team_Member__c.fHCM2__First_Name__c' => User::FIRST_NAME,
-            'fHCM2__Team_Member__c.fHCM2__Surname__c' => User::LAST_NAME,
-            'fHCM2__Team_Member__c.Name' => User::DISPLAY_NAME,
-            'User.Email' => User::EMAIL,
-            'User.Username' => User::USERNAME,
-    /** TODO: Inquire about the following two fields:
-            'Account_Locked__Disabled_or_Expired' => User::LOCKED,
-            'requireMfa' => User::REQUIRE_MFA,
-    */
-            'User.Manager.Email' => User::MANAGER_EMAIL,
-            'fHCM2__Team_Member__c.fHCM2__Home_Email__c' => User::PERSONAL_EMAIL,
+            'Name' => User::DISPLAY_NAME,
+            'fHCM2__Unique_Id__c' => User::EMPLOYEE_ID,
+            'fHCM2__First_Name__c' => User::FIRST_NAME,
+            'fHCM2__Surname__c' => User::LAST_NAME,
+            'fHCM2__Home_Email__c' => User::PERSONAL_EMAIL,
+            'fHCM2__User__r.Email' => User::EMAIL,
+            'fHCM2__User__r.Username' => User::USERNAME,
+            'fHCM2__Manager_User__r.Email' => User::MANAGER_EMAIL,
+//  TODO: Inquire about the following two fields:
+//            'Account_Locked__Disabled_or_Expired' => User::LOCKED,
+//            'requireMfa' => User::REQUIRE_MFA,
             // No 'active' needed, since all ID Store records returned are active.
         ];
     }
@@ -131,6 +131,21 @@ class SagePeopleIdStore extends IdStoreBase
     }
 
     /**
+     * Parse response body into a one-dimensional associative array of properties.
+     * @param array $body
+     * @return array
+     */
+    private function getRecordsFromResponseBody($body)
+    {
+        return  array_map(
+            function ($item) {
+                return Utils::arrayCollapseRecursive($item);
+            },
+            $body['records'] ?? []
+        );
+    }
+
+    /**
      * Get information about each of the (active) users.
      *
      * @return User[] A list of Users.
@@ -138,7 +153,52 @@ class SagePeopleIdStore extends IdStoreBase
      */
     public function getAllActiveUsers()
     {
-        throw new Exception(__FUNCTION__ . ' not yet implemented');
+        $accessToken = $this->getAccessToken();
+
+        $response = $this->getHttpClient()->get($this->queryUrl, [
+            'connect_timeout' => $this->timeout,
+            'headers' => [
+                'Accept' => 'application/json',
+                'Accept-Encoding' => 'gzip',
+                'Authorization' => 'Bearer ' . $accessToken,
+                'Content-Type' => 'application/json',
+            ],
+            'http_errors' => false,
+            'query' => [
+                'q' => 'SELECT Name,fHCM2__First_Name__c,fHCM2__Surname__c,fHCM2__User__r.Username,'
+                    . 'fHCM2__User__r.Email,fHCM2__Manager_User__r.Email,fHCM2__Unique_Id__c,fHCM2__Home_Email__c'
+                    . ' FROM fHCM2__Team_Member__c'
+                    . ' WHERE fHCM2__Employment_Status__c=\'Active\''
+                    . ' AND fHCM2__User__c!=null',
+            ],
+        ]);
+
+        $statusCode = (int)$response->getStatusCode();
+
+        if (($statusCode >= 400) && ($statusCode <= 499)) {
+            $body = null;
+        } elseif (($statusCode >= 200) && ($statusCode <= 299)) {
+            $body = Json::decode($response->getBody());
+        } else {
+            throw new Exception(sprintf(
+                'Unexpected response (%s %s): %s',
+                $response->getStatusCode(),
+                $response->getReasonPhrase(),
+                $response->getBody()
+            ), 1558380644);
+        }
+
+        if (! is_array($body)) {
+            echo PHP_EOL . json_encode($response->getBody()->getContents()) . PHP_EOL;
+            throw new Exception(sprintf(
+                'Unexpected result when getting all active users: %s',
+                var_export($body, true)
+            ), 1558380645);
+        }
+
+        $allActiveUsers = self::getRecordsFromResponseBody($body);
+
+        return self::getAsUsers($allActiveUsers);
     }
 
     /**
