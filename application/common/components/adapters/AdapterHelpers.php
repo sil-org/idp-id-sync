cea0d925a3b2f08558b675e7eeb21f002a034dee
Steve Bagwell <steve_bagwell@sil.org>
2022-09-06 15:47:51 -0400

    ensure a user from workday with a missing manager_email blanks it out

---
+++ application/common/components/adapters/AdapterHelpers.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Sil\Idp\IdSync\common\components\adapters;
+
+
+class AdapterHelpers
+{
+    /**
+     * Modifies array in place by adding a $property entry with a blank value if the $property key is not present.
+     * @param string $property the key of the entry to look for
+     * @param array $items (nested array)
+     */
+    public static function addBlankProperty(string $property, array &$items) {
+        foreach ($items as &$next) {
+            if (!isset($next[$property])) {
+                $next[$property] = '';
+            }
+        }
+    }
+}
