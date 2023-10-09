163028a9e1cb95640bb831bf9882619687bb6de5
briskt <3172830+briskt@users.noreply.github.com>
2023-10-09 15:18:14 -0600

    update version string to 4.5.1-pre

--- application/common/config/main.php
+++ application/common/config/main.php
@@ -131,7 +131,7 @@ return [
                     'clientOptions' => [
                         'attach_stacktrace' => false, // stack trace identifies the logger call stack, not helpful
                         'environment' => YII_ENV,
-                        'release' => 'idp-id-sync@4.5.0',
+                        'release' => 'idp-id-sync@4.5.1-pre',
                         'before_send' => function (Event $event) use ($idpName): ?Event {
                             $event->setExtra(['idp' => $idpName]);
                             return $event;
